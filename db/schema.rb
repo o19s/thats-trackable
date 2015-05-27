de4ecea241669e6f7ddbc41e0728b32e7d58db97
Logan-K <lklingler0@gmail.com>
2015-05-26 21:23:22 -0400

    Starting to add passwords to runners

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150527010726) do
+ActiveRecord::Schema.define(version: 20150527012030) do
 
   create_table "groups", force: :cascade do |t|
     t.string   "name"
@@ -31,9 +31,10 @@ ActiveRecord::Schema.define(version: 20150527010726) do
     t.string   "name"
     t.integer  "group_id"
     t.string   "role"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
     t.string   "username"
+    t.string   "password_digest"
   end
 
   create_table "runs", force: :cascade do |t|
