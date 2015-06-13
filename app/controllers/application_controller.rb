ed6c778c85d61bab281daee09bca0de0f7456f45
Eric Pugh <epugh@o19s.com>
2015-06-12 21:35:05 -0400

    redirect users to log in if they arent already logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,15 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   include SessionsHelper
+
+  before_action :require_login
+
+  private
+ 
+  def require_login
+    unless logged_in?
+      flash[:error] = "You must be logged in to access this section"
+      redirect_to login_path # halts request cycle
+    end
+  end
 end
