d7b77607c7b620a7aa5dc62fc9db0d8a7ed9fedc
Eric Pugh <epugh@o19s.com>
2015-06-16 06:35:00 -0400

    I realized I dont need the trim, cause I am keeping my data clean!!!!!!!   That is much better

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-    runner = Runner.find_by(email: params[:session][:email].downcase.strip)
+    runner = Runner.find_by(email: params[:session][:email].downcase)
     if runner && runner.authenticate(params[:session][:password])
       # Log the user in and redirect to the user's show page
       log_in runner
