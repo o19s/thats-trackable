fcec75f58685d90678bc553914c7d27f8fa297be
Eric Pugh <epugh@o19s.com>
2015-06-29 10:02:28 -0400

    Revert "one more try"
    This reverts commit 8657bf4a121cf2693534c4cef84cf063217eec79.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,12 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  #provider :facebook, Rails.application.secrets.facebook_app_id, Rails.application.secrets.facebook_app_secret, scope: 'public_profile'
-
-  puts "Here is debugging: Facebook ID: #{Rails.application.secrets.facebook_app_id}, Secret: #{Rails.application.secrets.facebook_app_secret}"
-  
-  provider :facebook, Rails.application.secrets.facebook_app_id, Rails.application.secrets.facebook_app_secret, { 
-    client_options: { ssl: { 
-        ca_file: '/usr/lib/ssl/certs/ca-certificates.crt',
-        ca_path: "/etc/ssl/certs"
-    }}
-  }
+  provider :facebook, Rails.application.secrets.facebook_app_id, Rails.application.secrets.facebook_app_secret, scope: 'public_profile'
 end
