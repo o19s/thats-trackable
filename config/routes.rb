a259cb2c4a060db9dca8c6ba0e5a591f813b6e09
Sparc0 <lklingler0@gmail.com>
2015-07-03 13:05:57 -0400

    Changed route back

--- config/routes.rb
+++ config/routes.rb
@@ -29,7 +29,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'sessions#new'
+  root 'sessions#today'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
