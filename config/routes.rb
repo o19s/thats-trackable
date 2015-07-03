f12922bbe1219aeb8dba2458e303b25697d05604
Sparc0 <lklingler0@gmail.com>
2015-07-03 11:38:53 -0400

    Added flash message for bad login AND CHANGED ROOT ROUTE TO SESSIONS#NEW!

--- config/routes.rb
+++ config/routes.rb
@@ -29,7 +29,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'sessions#today'
+  root 'sessions#new'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
