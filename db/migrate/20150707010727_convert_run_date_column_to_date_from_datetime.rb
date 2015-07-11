fa2430ceb10cf4121bf3356e4b07ae9b5698f734
Eric Pugh <epugh@o19s.com>
2015-07-11 17:18:06 -0400

    trying recommendation from SO for postgres

--- db/migrate/20150707010727_convert_run_date_column_to_date_from_datetime.rb
+++ db/migrate/20150707010727_convert_run_date_column_to_date_from_datetime.rb
@@ -1,5 +1,13 @@
 class ConvertRunDateColumnToDateFromDatetime < ActiveRecord::Migration
   def change
       change_column :runs, :date, :date
+
+      rename_column :runs, :date, :date_string
+      add_column :runs, :date, :date
+
+      Run.reset_column_information
+      Run.find_each { |run| run.update_attribute(:date, run.datestring) }
+      remove_column :runs, :date_string
+
   end
 end
