518e9d9758f8c77e559ef87eced7fa244ddb0447
Logan-K <lklingler0@gmail.com>
2015-08-15 16:25:03 -0400

    Test work. Still in development

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150713195549) do
+ActiveRecord::Schema.define(version: 20150714012236) do
 
   create_table "activities", force: :cascade do |t|
     t.integer  "trackable_id"
@@ -115,6 +115,7 @@ ActiveRecord::Schema.define(version: 20150713195549) do
     t.datetime "updated_at",     null: false
     t.integer  "planned_run_id"
     t.date     "date"
+    t.integer  "changed_flag"
   end
 
 end
--- test/models/runner_test.rb
+++ test/models/runner_test.rb
@@ -4,4 +4,30 @@ class RunnerTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  setup do
+    @runner = runners(:one)
+    @planned_run = planned_runs(:one)
+    @group = groups(:one)
+  end
+
+  test "should create runner" do
+    runner = Runner.new
+    assert_not runner.save #fail if runner cannot be created
+  end
+
+  test "should_have_planned_runs" do
+    #create runner
+    new_runner = Runner.create(id: @runner.id, name: @runner.name, group_id: @runner.group_id, role: @runner.role, email: @runner.email, password_digest: @runner.password_digest )
+    assert_not new_runner.save
+
+    # Making sure runner has runs
+    for i in new_runner.runs
+      count += 1
+    end
+
+    assert (count = 0)
+
+  end
+
 end
